3468d755372d1eba8300190919648740652d36a3
Suradi <s67510878@gmail.com>
2024-11-11 23:31:14 +0700

    Menambahkan pemeriksaan form submission dan pengambilan data pengguna (NIM dan password) ().

--- index.php
+++ index.php
@@ -4,3 +4,9 @@ session_start();
 //Hardcoded user data as an example(to be replaced with a database in a real application)
 $valid_nim="12345678";
 $valid_password="user123";
+
+//Check if form is submited
+if($_SERVER["REQUEST_METHOD"]=="POST"){
+    $nim=$_POST["nim"];
+    $password=$_POST["password"];
+}
