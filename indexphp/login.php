c86fe69b72da5167d47130fbcf1f48522f95724f
Suradi <s67510878@gmail.com>
2024-11-11 21:29:20 +0700

    Mengarahkan pengguna ke halaman login jika sesi belum diatur. ().

--- indexphp/login.php
+++ indexphp/login.php
@@ -3,5 +3,7 @@ session_star();
 
 //periksa apakah sesi'username'sudah ada
 if(!isset($_SESSION['username'])){
-    
+    //jika sesi tidak ada, arahkan ke halaman login
+    header("Location:index.php");
+    exit();
 }
