edc4dee07fa2dde2d4ec31bfd9cec2f3017665be
Suradi <s67510878@gmail.com>
2024-11-12 02:27:31 +0700

    memperbaiki code

--- exec/exec.php
+++ exec/exec.php
@@ -1,4 +1,3 @@
 <?php
-$localhost = "php -s localhost:800 -t C:\xampp\htdocs\Proyek Suradi"
-
+$localhost = "php -s localhost:8000 -t C:\xampp\htdocs\Proyek Suradi";
 shell_exec($localhost);
--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-ent.getElementById("error-msg"function validateForm(){
+function validateForm(){
     const nim=document.getElementById("nim").value;
     const password=document.getElementById("password").value;
     const errorMsg=document.getElementById("error-msg");
 
-    if(nim===""||pasword===""){
+    if(nim===""||password===""){
         errorMsg.textContent="NIM dan password harus diisi";
         return false
     }
