3dfec5d5d2d1210ba4067fdf22a5b7af8f0fdc76
Suradi <s67510878@gmail.com>
2024-11-12 00:48:50 +0700

    Menambahkan struktur dasar fungsi validasi form login mahasiswa dengan pengecekan input NIM dan password.. ().

---
+++ main.js
@@ -0,0 +1,6 @@
+ent.getElementById("error-msg"function validateForm(){
+    const nim=document.getElementById("nim").value;
+    const password=document.getElementById("password").value;
+    const errorMsg=document.getElementById("error-msg");
+
+}
